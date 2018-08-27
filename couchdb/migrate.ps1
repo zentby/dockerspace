6b4c49cf06a1b468de67c415a2fc9b368cdaa12a
Yang Zhao <danielz@unleashedsoftware.com>
2018-08-27 14:49:17 +1200

    Fix migration ps file Added restart policy in compose file

--- couchdb/migrate.ps1
+++ couchdb/migrate.ps1
@@ -1,5 +1,9 @@
 $path =  (pwd).path
-Invoke-Expression "docker run -d --name couch-bak -p 5988:5984 -v $path\local.ini:/opt/couchdb/etc/local.d/local.ini -v $path\data:/opt/couchdb/data klaemo/couchdb:latest"
+if (!(Test-Path $path\data))
+{
+    New-Item -ItemType directory -Path $path\data
+}
+Invoke-Expression "docker run -d --rm --name couch-bak -p 5988:5984 -v $path\local\local.ini:/opt/couchdb/etc/local.d/local.ini -v $path\data:/opt/couchdb/data klaemo/couchdb:latest"
 $command = 'docker inspect -f "{{ .NetworkSettings.Networks.bridge.Gateway }}" couch-bak'
 $ip = Invoke-Expression $command
 write-host "CouchDB Server IP: $ip"
@@ -7,4 +11,4 @@ Start-Process -FilePath "http://127.0.0.1:5988/_utils"
 write-host "Leave this open and migrate your data in the openning admin page"
 write-host "Press any key to continue to remove the temp container"
 [void][System.Console]::ReadKey($true)
-Invoke-Expression "docker rm -f couch-bak"
+Invoke-Expression "docker stop couch-bak"
